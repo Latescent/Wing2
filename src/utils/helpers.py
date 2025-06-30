483fcfe49626e9dcacab6db104678bc4b393ab44
Neutrallin4 <neutralobs@gmail.com>
2025-06-30 21:04:56 +0330

    Documentation for helpers.py. Related to #4, #9

--- src/utils/helpers.py
+++ src/utils/helpers.py
@@ -1,6 +1,22 @@
+"""A collection of tools that are required to help other scripts.
+"""
+
 import sys
 
-def progress_bar(total, current, prefix="", suffix=""):
+def progress_bar(total: int, current: int, prefix: str = "", suffix: str = "") -> None:
+    """Displays a text-based rogress bar.
+
+    Creates a text based progress bar. The effects are achieved with terminal text manipulation.
+
+    Args: 
+        total : The total number of tasks, representing 100% of the bar.
+        current : The number of tasks that have been completed.
+        prefix : An optional string to display at the beginning of the progress bar.
+        suffix : An optional string to display at the end of the progress bar.
+
+    Returns:
+        None
+    """
     bar_length = 50
     progress = float(current) / total
     arrow = '=' * int(round(progress * bar_length) - 1) + '>'
@@ -11,3 +27,4 @@ def progress_bar(total, current, prefix="", suffix=""):
     
     if current == total:
         sys.stdout.write("\n")
+
